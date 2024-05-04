23e0f416b654657050419bb290ece28b80a30ef0
enggipratama <36240898+enggipratama@users.noreply.github.com>
2024-05-05 00:05:35 +0700

    login & register page

--- resources/views/Register/register.blade.php
+++ resources/views/Register/register.blade.php
@@ -14,7 +14,7 @@
     <div class="hero min-h-screen bg-base">
         <div class="card lg:card-side bg-base-100 shadow-xl">
             <div class="card-body w-full">
-                <h2 class="mb-4 text-3xl font-bold text-center">Login</h2>
+                <h2 class="mb-4 text-3xl font-bold text-center">Register</h2>
                 <div class="flex justify-center mb-2">
                     <label class="input input-bordered flex items-center gap-2">
                         <i class="fa-solid fa-envelope"></i>
@@ -27,20 +27,20 @@
                         <input type="password" class="rounded-lg input-sm w-full" placeholder="Password" />
                     </label>
                 </div>
-                <div class="flex items-center mt-2">
-                    <input id="default-checkbox" type="checkbox" value=""
-                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                    <label for="default-checkbox"
-                        class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+                <div class="flex justify-center mb-2">
+                    <label class="input input-bordered flex items-center gap-2">
+                        <i class="fa-solid fa-user"></i>
+                        <input type="text" class="rounded-lg input-sm w-full" placeholder="Username" />
+                    </label>
+                </div>
+                <div class="flex justify-center mb-2">
+                    <label class="input input-bordered flex items-center gap-2">
+                        <i class="fa-solid fa-phone"></i>   
+                        <input type="number" class="rounded-lg input-sm w-full" placeholder="Phone" />
+                    </label>
                 </div>
                 <div class="flex items-center mt-5 justify-center">
-                    <button class="btn btn-primary">Login</button>
-                </div>
-                <div class="flex items-center mt-5 justify-center">
-                    <a href="#" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Forget Password?</a>
-                </div>
-                <div class="flex items-center justify-center">
-                    <a class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Don't have an account? <a class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300" href="{{ route('register') }}">Register</a>
+                    <button class="btn btn-primary">Register</button>
                 </div>
             </div>
             <div class="flex justify-center w-full">
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Login.login');
 });
+Route::get('/register', function () {
+    return view('Register.register');
+});
